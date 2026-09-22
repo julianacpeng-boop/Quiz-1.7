a88d233abc9c219c11478d5243afb9ee920e31bb
julianacpeng-boop <julianacp.eng@gmail.com>
2026-09-22 19:20:09 -0300

    Add files via upload

---
+++ dados_quiz_exemplo_imagem_opcional.py
@@ -0,0 +1,21 @@
+QUIZZES = {
+    "Enfermagem": [
+        {
+            "pergunta": "Quem ficou conhecida por ajudar a transformar a enfermagem em uma profissão moderna?",
+            "alternativas": ["Florence Nightingale", "Marie Curie", "Clara Barton"],
+            "correta": 0,
+            "imagem": "imagens_perguntas/florence.png",
+        },
+        {
+            "pergunta": "Em qual século a enfermagem moderna começou a se profissionalizar?",
+            "alternativas": ["Século XVII", "Século XIX", "Século XXI"],
+            "correta": 1,
+        },
+        {
+            "pergunta": "A escola criada por Florence Nightingale em 1860 funcionava ligada a qual instituição?",
+            "alternativas": ["Universidade", "Exército", "Hospital"],
+            "correta": 2,
+            "imagem": "florence_escola.webp",
+        },
+    ],
+}
